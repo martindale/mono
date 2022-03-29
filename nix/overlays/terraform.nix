e061d5f8bc8ffabd658a556a267e970064c0d8a5
Anand Suresh <anandsuresh@gmail.com>
2022-03-28 18:53:01 -0700

    add terraform to the development environment

---
+++ nix/overlays/terraform.nix
@@ -0,0 +1,24 @@
+self: super: {
+  terraform =
+    let
+      inherit (super) lib pkgs sources;
+
+      # validates a terraform provider based on the name of the package
+      isTfProvider = name: value: lib.hasPrefix "terraform-provider-" name;
+
+      # filters the (niv) sources to pick all terraform providers
+      tfProviders = lib.filterAttrs isTfProvider sources;
+
+      # generates a list of terraform plugins from the terraform providers
+      tfPlugins = lib.mapAttrsToList (pname: src: super.buildGoModule {
+        inherit pname src;
+        inherit (src) vendorSha256 version;
+
+        passthru = src;
+        postBuild = "mv $NIX_BUILD_TOP/go/bin/${src.repo}{,_v${src.version}}";
+        subPackages = [ "." ];
+      }) tfProviders;
+    in
+      super.terraform_1.withPlugins (_: tfPlugins ++ (with pkgs; [
+      ]));
+}
--- shell.nix
+++ shell.nix
@@ -7,11 +7,29 @@ in
       git
       less
       niv
+      terraform
       which
     ];
 
     shellHook = ''
       # Needed for macOS, since the TMPDIR path is long there
       export TMPDIR=/tmp
+
+      # Configuration file to setup
+      export PORTAL=$HOME/.config/portal.conf
+      if [ -f $PORTAL ]; then
+        source $PORTAL
+      else
+        echo "Developer environment not setup correctly!"
+        echo "Please setup your environment before proceeding by running:"
+        echo
+        echo "cat > $PORTAL <<-EOF"
+        echo "# This file sets up the security credentials for needed for"
+        echo "# deploying resources in the Portal Terraform code-base."
+        echo
+        echo "# Your settings/credentials go below."
+        echo "EOF"
+        exit 1
+      fi
     '';
   }
