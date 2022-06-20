6d6905f2b19926b413015c4b7892fdd7fe3aa48d
Anand Suresh <anandsuresh@gmail.com>
2022-06-20 12:40:50 -0700

    add `build-module` alias to help test nixos modules

--- shell.nix
+++ shell.nix
@@ -40,10 +40,15 @@ in
       fi
 
       # Aliases
+      ## Helpers
       alias ls='ls --color'
       alias l='ls -la'
 
+      ## Extracts the deployment key from Terraform
       alias tf-get-deploy-private-key='terraform state pull | jq -r ".resources[] | select(.name == \"deploy\") | .instances[0].attributes.private_key_pem"'
       alias tf-get-deploy-public-key='terraform state pull | jq -r ".resources[] | select(.name == \"deploy\") | .instances[0].attributes.public_key_openssh"'
+
+      ## Builds a NixOS system including the specified module; useful for testing
+      alias build-module='nix-build --attr nixos --argstr module'
     '';
   }
