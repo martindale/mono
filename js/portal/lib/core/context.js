16c756d22256d0586a3bcce8dd7c0d5ed022ee9c
Anand Suresh <anandsuresh@gmail.com>
2022-09-20 11:21:19 -0700

    initial commit with @melnx

--- js/portal/lib/core/context.js
+++ js/portal/lib/core/context.js
@@ -3,12 +3,15 @@
  */
 
 const Store = require('./store')
+const Web3Legacy = require('web3-legacy')
 
 /**
  * Export an object that exposes the injected dependencies
  * @type {Object}
  */
 module.exports = {
+  goerli: new Web3Legacy(process.env.PORTAL_GOERLI_RPC_URL),
   log: console,
+  ropsten: new Web3Legacy(process.env.PORTAL_ROPSTEN_RPC_URL),
   store: new Store({ path: process.env.PORTAL_STORE_PATH })
 }
---
+++ js/portal/test/api/v1/swap/index.js
@@ -0,0 +1,9 @@
+/**
+ * @file Behavioral spec for the orderbook
+ */
+
+module.exports = function (client, server) {
+  describe('Swap', function () {
+    it('must create a new order in the order book')
+  })
+}
